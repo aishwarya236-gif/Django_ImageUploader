d1ecf2c53c72ab2a76eca1ee25684e459c13360b
aishwarya236-gif <78917781+aishwarya236-gif@users.noreply.github.com>
2021-07-21 16:41:00 +0530

    Update wsgi.py

--- imageuploader/wsgi.py
+++ imageuploader/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imageuploder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imageuploader.settings')
 
 application = get_wsgi_application()
