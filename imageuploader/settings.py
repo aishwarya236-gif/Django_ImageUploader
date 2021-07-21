0faf5a91dedcb0b1d647d8716520ef5556cb1460
aishwarya236-gif <78917781+aishwarya236-gif@users.noreply.github.com>
2021-07-21 16:40:06 +0530

    Update settings.py

--- imageuploader/settings.py
+++ imageuploader/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ap!y+mc3sazcp&yl4*j=^y@43=z-a+n16dnao6bn034&*g31*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["image-uploader-236.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
